1cfcac6810f3b32e6983c1ac091b760acc334ac1
rishab agarwal <rishabagarwal7594@gmail.com>
2019-01-31 23:35:22 +0530

    Updates

---
+++ precache-manifest.4049e3ecdc3dd765267bc478bf95d283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f69f883c56ea23d29a7526752e43b76d",
+    "url": "/weather-forecast/static/media/background.f69f883c.jpg"
+  },
+  {
+    "revision": "f2d38ab6dc1f45e8eef0",
+    "url": "/weather-forecast/static/js/runtime~main.f2d38ab6.js"
+  },
+  {
+    "revision": "d2d6855f668df12589f3",
+    "url": "/weather-forecast/static/js/main.d2d6855f.chunk.js"
+  },
+  {
+    "revision": "6454d164ec34cd2312b8",
+    "url": "/weather-forecast/static/js/1.6454d164.chunk.js"
+  },
+  {
+    "revision": "d2d6855f668df12589f3",
+    "url": "/weather-forecast/static/css/main.b4467d3b.chunk.css"
+  },
+  {
+    "revision": "3a51ca1b35104cbf5c559c45b3ea006b",
+    "url": "/weather-forecast/index.html"
+  }
+];
